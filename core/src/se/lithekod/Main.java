cc532fe0044125b4084d7ea123fe1741d58ddf67
Christoffer <chsnot@gmail.com>
2015-04-18 16:24:54 +0200

    Rabbits

--- core/src/se/lithekod/Main.java
+++ core/src/se/lithekod/Main.java
@@ -9,12 +9,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+import java.util.ArrayList;
+
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture playerImg;
+	Texture rabbitImg;
     Map map;
     Player player;
+	ArrayList<Rabbit> rabbits;
     Sprite playerSprite;
+	Sprite rabbitSprite;
 	Texture soil;
 	Texture digged;
 	public static int DESKTOP_HEIGHT;
@@ -22,10 +27,12 @@ public class Main extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+		rabbits = new ArrayList<Rabbit>();
 		DESKTOP_HEIGHT = Gdx.graphics.getHeight();
 		DESKTOP_WIDTH = Gdx.graphics.getWidth();
 		batch = new SpriteBatch();
 		playerImg = new Texture("mole_original.png");
+		rabbitImg = new Texture("rabbit_sheet.png");
 		soil = new Texture("Soil.png");
 		digged = new Texture("Soil_digged.png");
 		soil.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
@@ -33,6 +40,8 @@ public class Main extends ApplicationAdapter {
         this.map = new Map();
         this.player = new Player();
         playerSprite = new Sprite(playerImg);
+		this.rabbits.add(new Rabbit());
+		rabbitSprite = new Sprite(rabbitImg);
 		InputHandler inputHandler = new InputHandler();
 		Gdx.input.setInputProcessor(inputHandler);
 	}
@@ -40,6 +49,10 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void render () {
         player.update();
+
+		for (Rabbit r : rabbits){
+			r.update();
+		}
 
 		Gdx.gl.glClearColor(.1f, .7f, .99f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -56,6 +69,8 @@ public class Main extends ApplicationAdapter {
 
 		playerSprite.setPosition(badgerPositionX - playerSprite.getWidth() / 2, badgerPositionY - playerSprite.getHeight() / 2);
 		playerSprite.setRotation(player.getRotation());
+
+		rabbitSprite.setPosition(20, 20);
 
 		// Set map is cleared
 		map.setCleared(badgerPositionX, badgerPositionY);
@@ -77,6 +92,7 @@ public class Main extends ApplicationAdapter {
         //playerSprite.rotate(.5f);
 //        playerSprite.getRotation()
         playerSprite.draw(batch);
+		rabbitSprite.draw(batch);
 		batch.end();
 	}
 
