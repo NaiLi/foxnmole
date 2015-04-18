22f82e8b14b1ad5b9437aa6bfd2751c99041717c
NaiLi <linnea.nabo@gmail.com>
2015-04-18 18:26:09 +0200

    lots of rabbits!

--- core/src/se/lithekod/Main.java
+++ core/src/se/lithekod/Main.java
@@ -15,163 +15,172 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture playerImg;
+    SpriteBatch batch;
+    Texture playerImg;
     public static Map map;
     Player player;
     public static Sprite playerSprite;
-	Texture rabbitImg;
-	ArrayList<Rabbit> rabbits;
-	Sprite rabbitSprite;
-	Texture soil;
-	Texture digged;
-	public static int DESKTOP_HEIGHT;
-	public static int DESKTOP_WIDTH;
-	public static Pixmap pixmap;
-	public static Pixmap diggedMap;
-	
-	@Override
-	public void create () {
-		rabbits = new ArrayList<Rabbit>();
-		DESKTOP_HEIGHT = Gdx.graphics.getHeight();
-		DESKTOP_WIDTH = Gdx.graphics.getWidth();
-		batch = new SpriteBatch();
-		playerImg = new Texture("mole_original.png");
-		rabbitImg = new Texture("rabbit_sheet_single.png");
-		soil = new Texture("Soil.png");
-		digged = new Texture("Soil_digged.png");
-		soil.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
-		digged.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+    Texture rabbitImg;
+    ArrayList<Rabbit> rabbits;
+    ArrayList<Sprite> rabbitSprites;
+    Texture soil;
+    Texture digged;
+    public static int DESKTOP_HEIGHT;
+    public static int DESKTOP_WIDTH;
+    public static Pixmap pixmap;
+    public static Pixmap diggedMap;
+    private int count = 0;
+
+    @Override
+    public void create() {
+        rabbits = new ArrayList<Rabbit>();
+        DESKTOP_HEIGHT = Gdx.graphics.getHeight();
+        DESKTOP_WIDTH = Gdx.graphics.getWidth();
+        batch = new SpriteBatch();
+        playerImg = new Texture("mole_original.png");
+        rabbitImg = new Texture("rabbit_sheet_single.png");
+        soil = new Texture("Soil.png");
+        digged = new Texture("Soil_digged.png");
+        soil.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        digged.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
         this.map = new Map();
         this.player = new Player();
         playerSprite = new Sprite(playerImg);
-		this.rabbits.add(new Rabbit());
-		rabbitSprite = new Sprite(rabbitImg);
-		InputHandler inputHandler = new InputHandler();
-		Gdx.input.setInputProcessor(inputHandler);
+        this.rabbits.add(new Rabbit(1));
+        InputHandler inputHandler = new InputHandler();
+        Gdx.input.setInputProcessor(inputHandler);
 
 
-		pixmap = new Pixmap(DESKTOP_WIDTH, DESKTOP_HEIGHT - Map.SKY_HEIGHT, Pixmap.Format.RGBA8888);
-		pixmap.setColor(.1f, .7f, .99f, 1);
-		pixmap.fill();
-		Pixmap soilMap = new Pixmap(Gdx.files.internal("Soil.png"));
-		diggedMap = new Pixmap(Gdx.files.internal("Soil_digged.png"));
-		for (int i = 0; i < DESKTOP_WIDTH; i += soilMap.getWidth()) {
-			for (int j = 0; j < DESKTOP_HEIGHT - Map.SKY_HEIGHT; j += soilMap.getHeight()) {
-				pixmap.drawPixmap(soilMap, i,j);
-			}
-		}
-		pixmap.setColor(0, 0, 0, 0);
-	}
+        pixmap = new Pixmap(DESKTOP_WIDTH, DESKTOP_HEIGHT - Map.SKY_HEIGHT, Pixmap.Format.RGBA8888);
+        pixmap.setColor(.1f, .7f, .99f, 1);
+        pixmap.fill();
+        Pixmap soilMap = new Pixmap(Gdx.files.internal("Soil.png"));
+        diggedMap = new Pixmap(Gdx.files.internal("Soil_digged.png"));
+        for (int i = 0; i < DESKTOP_WIDTH; i += soilMap.getWidth()) {
+            for (int j = 0; j < DESKTOP_HEIGHT - Map.SKY_HEIGHT; j += soilMap.getHeight()) {
+                pixmap.drawPixmap(soilMap, i, j);
+            }
+        }
+        pixmap.setColor(0, 0, 0, 0);
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render() {
         player.update();
-		
-		for (Rabbit r : rabbits){
-			r.update();
-		}
 
-		Gdx.gl.glClearColor(.1f, .7f, .99f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+        for (Rabbit r : rabbits) {
+            r.update();
+        }
 
-		// Check so badger dont go outside of dirt
-		float badgerPositionX = player.getPos().x;
-		badgerPositionX = (badgerPositionX < playerSprite.getWidth() / 2) ? playerSprite.getWidth() / 2 : badgerPositionX;
-		badgerPositionX = (badgerPositionX > DESKTOP_WIDTH - playerSprite.getWidth() / 2) ? (float) DESKTOP_WIDTH - playerSprite.getWidth() / 2: badgerPositionX;
-		float badgerPositionY = player.getPos().y;
-		badgerPositionY = (badgerPositionY < playerSprite.getHeight() / 2) ? playerSprite.getHeight() / 2 : badgerPositionY;
-		badgerPositionY = (badgerPositionY > DESKTOP_HEIGHT - playerSprite.getHeight() / 2 - map.getSkyHeight()) ? (float) DESKTOP_HEIGHT - playerSprite.getHeight() / 2 - map.getSkyHeight(): badgerPositionY;
-		player.setPos(badgerPositionX, badgerPositionY);
+        Gdx.gl.glClearColor(.1f, .7f, .99f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
 
-		playerSprite.setPosition(badgerPositionX - playerSprite.getWidth() / 2, badgerPositionY - playerSprite.getHeight() / 2);
-		playerSprite.setRotation(player.getRotation());
+        // Check so badger dont go outside of dirt
+        float badgerPositionX = player.getPos().x;
+        badgerPositionX = (badgerPositionX < playerSprite.getWidth() / 2) ? playerSprite.getWidth() / 2 : badgerPositionX;
+        badgerPositionX = (badgerPositionX > DESKTOP_WIDTH - playerSprite.getWidth() / 2) ? (float) DESKTOP_WIDTH - playerSprite.getWidth() / 2 : badgerPositionX;
+        float badgerPositionY = player.getPos().y;
+        badgerPositionY = (badgerPositionY < playerSprite.getHeight() / 2) ? playerSprite.getHeight() / 2 : badgerPositionY;
+        badgerPositionY = (badgerPositionY > DESKTOP_HEIGHT - playerSprite.getHeight() / 2 - map.getSkyHeight()) ? (float) DESKTOP_HEIGHT - playerSprite.getHeight() / 2 - map.getSkyHeight() : badgerPositionY;
+        player.setPos(badgerPositionX, badgerPositionY);
 
-		rabbitSprite.setPosition(rabbits.get(0).getPos().x - rabbitSprite.getWidth()/2, rabbits.get(0).getPos().y - rabbitSprite.getHeight()/2);
+        playerSprite.setPosition(badgerPositionX - playerSprite.getWidth() / 2, badgerPositionY - playerSprite.getHeight() / 2);
+        playerSprite.setRotation(player.getRotation());
 
-		// Set map is cleared
-		map.setCleared(badgerPositionX, badgerPositionY);
+        if(count%100 == 0) {
+            int dir = (count%3 == 0) ? 1 : -1;
+            Rabbit r = new Rabbit(dir);
+            rabbits.add(r);
+        }
 
-		if (player.getRotation() > 90 && player.getRotation() < 270)
-			playerSprite.setFlip(false, true);
-		else playerSprite.setFlip(false, false);
-		Texture tex = new Texture(pixmap);
-		batch.draw(tex, 0, 0);
+        // Set map is cleared
+        map.setCleared(badgerPositionX, badgerPositionY);
+
+        if (player.getRotation() > 90 && player.getRotation() < 270) {
+            playerSprite.setFlip(false, true);
+        }
+        else playerSprite.setFlip(false, false);
+        Texture tex = new Texture(pixmap);
+        batch.draw(tex, 0, 0);
         playerSprite.draw(batch);
-		rabbitSprite.draw(batch);
-		batch.end();
-	}
+        for(int i = 0; i < rabbits.size(); i++) {
+            batch.draw(rabbitImg, rabbits.get(i).getPos().x, rabbits.get(i).getPos().y);
+        }
+        batch.end();
 
-	public class InputHandler implements InputProcessor {
-		@Override
-		public boolean keyDown(int keycode) {
-			switch (keycode){
-				case Input.Keys.UP:
-					player.setUpPressed(true);
-					return true;
-				case Input.Keys.DOWN:
-					player.setDownPressed(true);
-					return true;
-				case Input.Keys.LEFT:
-					player.setLeftPressed(true);
-					return true;
-				case Input.Keys.RIGHT:
-					player.setRightPressed(true);
-					return true;
-				default:
-					return false;
-			}
-		}
+        count++;
+    }
 
-		@Override
-		public boolean keyUp(int keycode) {
-			switch (keycode){
-				case Input.Keys.UP:
-					player.setUpPressed(false);
-					return true;
-				case Input.Keys.DOWN:
-					player.setDownPressed(false);
-					return true;
-				case Input.Keys.LEFT:
-					player.setLeftPressed(false);
-					return true;
-				case Input.Keys.RIGHT:
-					player.setRightPressed(false);
-					return true;
-				default:
-					return false;
-			}
-		}
+    public class InputHandler implements InputProcessor {
+        @Override
+        public boolean keyDown(int keycode) {
+            switch (keycode) {
+                case Input.Keys.UP:
+                    player.setUpPressed(true);
+                    return true;
+                case Input.Keys.DOWN:
+                    player.setDownPressed(true);
+                    return true;
+                case Input.Keys.LEFT:
+                    player.setLeftPressed(true);
+                    return true;
+                case Input.Keys.RIGHT:
+                    player.setRightPressed(true);
+                    return true;
+                default:
+                    return false;
+            }
+        }
 
-		@Override
-		public boolean keyTyped(char character) {
-			return false;
-		}
+        @Override
+        public boolean keyUp(int keycode) {
+            switch (keycode) {
+                case Input.Keys.UP:
+                    player.setUpPressed(false);
+                    return true;
+                case Input.Keys.DOWN:
+                    player.setDownPressed(false);
+                    return true;
+                case Input.Keys.LEFT:
+                    player.setLeftPressed(false);
+                    return true;
+                case Input.Keys.RIGHT:
+                    player.setRightPressed(false);
+                    return true;
+                default:
+                    return false;
+            }
+        }
 
-		@Override
-		public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-			return false;
-		}
+        @Override
+        public boolean keyTyped(char character) {
+            return false;
+        }
 
-		@Override
-		public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-			return false;
-		}
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            return false;
+        }
 
-		@Override
-		public boolean touchDragged(int screenX, int screenY, int pointer) {
-			return false;
-		}
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            return false;
+        }
 
-		@Override
-		public boolean mouseMoved(int screenX, int screenY) {
-			return false;
-		}
+        @Override
+        public boolean touchDragged(int screenX, int screenY, int pointer) {
+            return false;
+        }
 
-		@Override
-		public boolean scrolled(int amount) {
-			return false;
-		}
-	}
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) {
+            return false;
+        }
+
+        @Override
+        public boolean scrolled(int amount) {
+            return false;
+        }
+    }
 }
--- core/src/se/lithekod/Rabbit.java
+++ core/src/se/lithekod/Rabbit.java
@@ -4,23 +4,35 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Rabbit {
     private float angle = 0;
-    private Vector2 pos = new Vector2(Main.DESKTOP_WIDTH-100, Main.DESKTOP_HEIGHT-Map.SKY_HEIGHT-10);
+    private Vector2 pos = new Vector2(Main.DESKTOP_WIDTH - 100, Main.DESKTOP_HEIGHT - Map.SKY_HEIGHT - 10);
     private boolean onGround = true;
+    private int direction;
+
+
+    public Rabbit(int direction) {
+        this.direction = direction;
+        if (direction > 0) { // walk right
+            pos.x = 0;
+        } else {
+            pos.x = Main.DESKTOP_WIDTH - 100;
+        }
+        pos.y = Main.DESKTOP_HEIGHT - Map.SKY_HEIGHT - 10;
+    }
 
     public void update() {
 
-        // Check if there is a hole below
-        if (Map.isCleared ( pos.x, pos.y-1 )) {
+        if (Map.isCleared(pos.x, pos.y - 1)) {
             pos.y = pos.y - 1;
             onGround = false;
-        } else if (Map.isCleared ( pos.x-1, pos.y )) {
-            pos.x = pos.x - 1;
+        } else if (Map.isCleared(pos.x + direction, pos.y)) {
+            pos.x = pos.x + direction;
         } else if (onGround) {
-            pos.x = pos.x - 1;
+            pos.x = pos.x + direction;
         }
 
-        // Else move forward
+        if(Map.isOutOfBounds(pos.x, pos.y)) {
 
+        }
     }
 
     public Vector2 getPos() {
